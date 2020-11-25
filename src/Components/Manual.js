7a89277afee13a2a7b535d6d8900d6114b537bce
Anjan Kumar <anjank81@gmail.com>
2020-11-25 23:04:40 +0530

    Manual changes

--- src/Components/Manual.js
+++ src/Components/Manual.js
@@ -7,6 +7,9 @@ import styles from './Manual.module.css';
 import { Checkbox } from '@material-ui/core';
 import Navigation from './Navigation';
 import Modal from './Modal';
+import DatePicker from 'react-datepicker';
+import 'rc-time-picker/assets/index.css';
+import TimePicker from 'rc-time-picker';
 const userDetails = Cookie.getCookie(USER_DETAILS);
 let parsedData = userDetails && JSON.parse(userDetails);
 
@@ -26,11 +29,14 @@ export default class Manual extends Component {
   componentDidMount() {
     this.props.postManualTimecard({ myManual: true });
   }
+  handleModal = () => {
+    this.setState({ manualModal: true });
+  };
   showModal = (val) => {
     this.setState({ show: true, type: val });
   };
   hideModal = () => {
-    this.setState({ show: false });
+    this.setState({ show: false, manualModal: false });
   };
   handleManualtimecard = (flag) => {
     if (flag == 0) {
@@ -85,6 +91,70 @@ export default class Manual extends Component {
     console.log(this.props);
     return (
       <div className={styles.base}>
+        <Modal
+          show={this.state.manualModal}
+          handleClose={this.hideModal}
+          width="auto"
+        >
+          <div className={styles.manualContainer}>
+            {this.state.manualError && (
+              <div style={{ color: 'red' }}>Check the time and try again</div>
+            )}
+            <div className={styles.dateHolder}>
+              <div className={styles.dateLabel}>Date</div>
+              <div className={styles.datePicker}>
+                <DatePicker
+                  selected={this.state.date}
+                  dateFormat={'yyyy-MM-dd'}
+                  onChange={(date) => this.onDateChange(date)}
+                  maxDate={new Date()}
+                  className={styles.dateFormat}
+                />
+              </div>
+            </div>
+            <div className={styles.dateHolder}>
+              <div className={styles.dateLabel}>Start Time</div>
+              <div className={styles.datePicker}>
+                <TimePicker
+                  onChange={this.handleStartTime}
+                  value={this.state.startTime}
+                  showSecond={false}
+                  minuteStep={10}
+                  // use12Hours={true}
+                />
+              </div>
+            </div>
+            <div className={styles.dateHolder}>
+              <div className={styles.dateLabel}>End Time</div>
+              <div className={styles.datePicker}>
+                <TimePicker
+                  onChange={this.handleEndTime}
+                  value={this.state.EndTime}
+                  showSecond={false}
+                  minuteStep={10}
+                  // use12Hours={true}
+                />
+              </div>
+            </div>
+            <div className={styles.dateHolder}>
+              <div className={styles.dateLabel}>Reason</div>
+              <div className={styles.datePicker}>
+                <textarea
+                  placeholder="Enter comments"
+                  row="3"
+                  className={styles.disputeComment}
+                  value={this.state.requestMessage}
+                  onChange={(e) => {
+                    this.setState({ requestMessage: e.target.value });
+                  }}
+                />
+              </div>
+            </div>
+            <div>
+              <button onClick={this.manualHandler}> Submit</button>
+            </div>
+          </div>
+        </Modal>
         <Modal show={this.state.show} handleClose={this.hideModal} width="auto">
           <div>
             <textarea
@@ -113,8 +183,7 @@ export default class Manual extends Component {
         <div className={styles.manualBase}>
           <div
             onClick={() => {
-              this.setState({ add: !this.state.add });
-              this.props.postNewCompany();
+              this.handleModal();
             }}
             className={styles.addButton}
           >
